3bafba8754b41693fb7cfeb382b5c8e71ff255cd
Jenkins YourRequest <support@yourrequest.nl>
2021-10-06 12:37:07 +0200

    Versie 0.0.1

---
+++ form-renderer/assets/script/analytics.js
@@ -0,0 +1,8 @@
+(function() {
+    var sz = document.createElement('script');
+    sz.type = 'text/javascript';
+    sz.async = true;
+    sz.src = 'https://siteimproveanalytics.com/js/siteanalyze_6006118.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(sz, s);
+})();
