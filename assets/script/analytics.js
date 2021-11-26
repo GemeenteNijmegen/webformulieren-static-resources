bd9b5b9a32c5e1eaf291512cc4f6e4228dcb2bdc
Jenkins YourRequest <support@yourrequest.nl>
2021-11-26 16:01:45 +0100

    Versie 1.2.0

--- assets/script/analytics.js
+++ assets/script/analytics.js
@@ -2,7 +2,7 @@
     var sz = document.createElement('script');
     sz.type = 'text/javascript';
     sz.async = true;
-    sz.src = 'https://siteimproveanalytics.com/js/siteanalyze_6006118.js';
+    sz.src = '/assets/script/siteimprove-analytics.js';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(sz, s);
 })();
