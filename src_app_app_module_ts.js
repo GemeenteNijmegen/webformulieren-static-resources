41f046c60cffbee451b8024db1e514bf8cfc9fd3
Jenkins YourRequest <support@yourrequest.nl>
2024-08-12 10:37:08 +0200

    Versie 1.27.0

--- src_app_app_module_ts.js
+++ src_app_app_module_ts.js
@@ -19808,7 +19808,8 @@ class FormComponent {
       if (currentComponent.path === key) {
         return currentComponent;
       } else if (currentComponent.components?.length > 0) {
-        return this.findComponentBySubmissionKey(key, currentComponent.components);
+        const found = this.findComponentBySubmissionKey(key, currentComponent.components);
+        if (found) return found;
       }
     }
     return null;
